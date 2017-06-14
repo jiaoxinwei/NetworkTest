4449af4d19f51d855b6aae3be9cffd84dcf0822b
茭白 <jiaoxinwei@myweimai.com>
2017-06-14 10:14:07 +0800

    1：项目的初建。

---
+++ NetworkTest/NetworkTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NetworkTest
+//
+//  Created by 茭白 on 2017/6/14.
+//  Copyright © 2017年 茭白. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NetworkTest/NetworkTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NetworkTest
+//
+//  Created by 茭白 on 2017/6/14.
+//  Copyright © 2017年 茭白. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
